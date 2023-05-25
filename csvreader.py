bf0dfb61bad5bed085ce56868c6e9323e8190533
Tristan678 <tristan.ganahl@gmail.com>
2023-05-25 17:36:39 +0200

    debugged category and transaction reader

--- csvreader.py
+++ csvreader.py
@@ -17,37 +17,39 @@ def read_csv_file(file_path, delimiter=';'):
             data.append(row)
         return data
 
-category_data = read_csv_file('Categories.csv')
-category_dict = {}
-for row in category_data:
-    print(row)
-    category_dict[row['\ufeffDescription']] = row['Category']
-
-transaction_data = read_csv_file('Transactions.csv')
-transaction_list = []
-for row in transaction_data:
-    print(row)
-    transaction_list.append((row['\ufeffDate'], row['Description'], row['Amount']))
-
-for i in range(len(transaction_list)):
-    category = category_dict.get(transaction_list[i][1], '')
-    transaction_list[i] = transaction_list[i] + (category,)
-
-def sort_by_category(x):
-    return x[3]
-
-transaction_list.sort(key=sort_by_category)
-
-current_category = ''
-for transaction in transaction_list:
-    if transaction[3] != current_category:
-        print('====={}====='.format(transaction[3]))
-        current_category = transaction[3]
-    print('{}   {}   {:.2f}'.format(datetime.strftime(transaction[0], '%d/%m/%Y'), transaction[1], transaction[2]))
-
-category_totals = {}
-for transaction in transaction_list:
-    category_totals[transaction[3]] = category_totals.get(transaction[3], 0) + transaction[2]
+def getExpCat():
     
-for category, total in category_totals.items():
-    print('{}: {:.2f}'.format(category, total))
+    category_data = read_csv_file('Categories.csv')
+    category_dict = {}
+    for row in category_data:
+        print(row)
+        category_dict[row['\ufeffDescription']] = row['Category']
+    
+    transaction_data = read_csv_file('Transactions.csv')
+    transaction_list = []
+    for row in transaction_data:
+        print(row)
+        transaction_list.append((row['\ufeffDate'], row['Description'], row['Amount']))
+    
+    for i in range(len(transaction_list)):
+        category = category_dict.get(transaction_list[i][1], '')
+        transaction_list[i] = transaction_list[i] + (category,)
+
+    def sort_by_category(x):
+        return x[3]
+    
+    transaction_list.sort(key=sort_by_category)
+            
+    current_category = ''
+    for transaction in transaction_list:
+                if transaction[3] != current_category:
+                    print('====={}====='.format(transaction[3]))
+                    current_category = transaction[3]
+                print('{}   {}   {:.2f}'.format(datetime.strftime(transaction[0], '%d/%m/%Y'), transaction[1], transaction[2]))
+            
+    category_totals = {}
+    for transaction in transaction_list:
+                category_totals[transaction[3]] = category_totals.get(transaction[3], 0) + transaction[2]
+                
+    for category, total in category_totals.items():
+                print('{}: {:.2f}'.format(category, total))
