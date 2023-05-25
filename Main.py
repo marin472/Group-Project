9e30f4a609ec394eb8d1d3a175747b006acadd91
Tristan678 <tristan.ganahl@gmail.com>
2023-05-25 16:31:58 +0200

    categories read

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ def show_menu():
 def expenses_menu():
     print("\n===== Expenses Menu =====")
     # Add your expenses-related options here
-    csvreader.read_csv_file("Expense")
+    csvreader.read_csv_file("Categories")
 
 def savings_goals_menu():
     print("\n===== Savings Goals Menu =====")
